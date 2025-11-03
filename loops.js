948c297ad1437230f69f4ef5fc9b4546fd137f74
unknown <joshuajacobs717@gmail.com>
2025-11-03 12:04:32 +0200

    Kaboom

---
+++ loops.js
@@ -0,0 +1,31 @@
+// TODO: Create an array called numbers with values 1 through 5
+const numbers = [1, 2, 3, 4, 5]
+// TODO: Write a for loop that prints each number in the array
+console.log("Here are the numbers in the array: ")
+for (number of numbers)
+    {
+    console.log(number)
+    }
+console.log("-".repeat(60))
+// TODO: Write a while loop that counts down from 5 to 1
+let countdown = 5
+while (countdown > 0)
+    {
+    console.log("Self destruct in: " + countdown); countdown--
+    }
+
+console.log("💥")
+// TODO: Create a loop that prints only even numbers from the numbers array
+console.log("Even numbers from array: ")
+for (number of numbers)
+    {
+    if (number % 2 == 0){console.log( number)}
+    }
+console.log("-".repeat(60))
+// TODO: Create a loop that calculates the sum of all numbers in the array
+let sum = 0
+for (number of numbers)
+    {
+    sum += number
+    }
+console.log("Here is the sum of the numbers in the array: " + sum)
